5706fa09c46fd2911b1f92ab14891efca4a15ef9
Colby Patterson <cpatt@byui.edu>
2022-06-28 12:29:22 -0600

    Properties controller

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,4 +1,17 @@
 class Property < ApplicationRecord
   belongs_to :agent
   has_one :address
+
+  def self.available
+    Property.find_by_sql("SELECT ag.first_name, ag.last_name, ag.email, p.id as property_id, p.beds, p.baths, p.sq_ft, p.price, a.street, a.id as address_id
+    FROM properties AS p
+    INNER JOIN addresses AS a
+    ON p.id = a.property_id
+    INNER JOIN agents as ag
+    ON ag.id = p.agent_id
+    WHERE p.SOLD <> TRUE;")
+  end
+
+  # def available
+  # end
 end
